d9882456d6fd5476c4d3a61e7b8a9fc6d26a1f7e
Rafael Marcelo Reimberg <raaafael.reimberg@hotmail.com>
2025-02-18 23:23:57 -0300

    ajustado o env e config.ts

--- node-login-dev/src/config.ts
+++ node-login-dev/src/config.ts
@@ -4,4 +4,10 @@ dotenv.config()
 
 export default {
   port: Number(process.env.PORT) || 3000,
+  auth: {
+    secret: process.env.AUTH_SECRET || 'secret',
+    expiresIn: process.env.AUTH_EXPIRES_IN || '7d',
+  },
 }
+
+// video stop 36:01
