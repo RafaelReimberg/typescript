2bbf5ae9efd290fc67df6a2cd565116f26f6991b
rafaelreimerbg <raaafael.reimberg@hotmail.com>
2025-03-18 17:39:42 -0300

    Update routes.ts

--- node-login-dev/src/app/Auth/routes.ts
+++ node-login-dev/src/app/Auth/routes.ts
@@ -5,6 +5,6 @@ import auth from '@app/Auth/controllers/AuthController'
 
 const routes = Router()
 
-routes.post('/auth/sign-in', auth.create)
+routes.post('/auth/sign-in', (req, res) => auth.create(req, res))
 
 export default routes
