10656763c95a7ab6de8f01311d408e097f34329e
Rafael Marcelo Reimberg <50966566+RafaelReimberg@users.noreply.github.com>
2025-06-16 15:23:15 -0300

    Teste em typescript

--- new-projeto/src/teste.js
+++ new-projeto/src/teste.js
@@ -1,1 +1,14 @@
-console.log('OLá Reimberg');
+var num = 8;
+// for (let i: number = 0; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+while (num <= 10) {
+    console.log("O n\u00FAmero \u00E9 ".concat(num));
+    num++;
+}
+// let idade: number = 18;
+// if (idade <= 18) {
+//     console.log("Menor de idade");
+// }else {
+//     console.log("Maior de idade");
+// }
--- new-projeto/src/teste.ts
+++ new-projeto/src/teste.ts
@@ -1,1 +1,19 @@
-console.log('Olá Reimberg');
+
+let num: number = 8;
+
+// for (let i: number = 0; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+while (num <= 10) {
+    console.log(`O número é ${num}`);
+    num++;
+}
+
+// let idade: number = 18;
+
+// if (idade <= 18) {
+//     console.log("Menor de idade");
+// }else {
+//     console.log("Maior de idade");
+// }
